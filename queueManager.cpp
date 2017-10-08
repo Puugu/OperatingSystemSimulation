446007740729e8848ff9f77ec2c018052f610037
Puugu <therealckirkham@gmail.com>
2017-10-08 15:29:55 -0500

    create method intros in header file

--- queueManager.cpp
+++ queueManager.cpp
@@ -2,7 +2,20 @@
 These are the methods for managing the queues.
 Puugu
 Project Created: 7 October 2017
-Last Edited: 7 October 2017
+Last Edited: 8 October 2017
 *****************************************************************************************************/
 
 #include "queueManager.h"
+
+using namespace std;
+
+//constructor
+queueManager::queueManager() {
+	/*Hey Future - Puugu, I know you always forget, so when you're looking back at old code trying
+	to figure out why your constructor is giving you an error about explicit type, YOUR FUNCTION
+	NAME HAS TO BE THE SAME AS YOUR CLASS NAME (when making the constructor and desctructor) */
+	head = NULL;
+	current = NULL;
+	tail = NULL;
+	godzilla = NULL;
+}
--- queueManager.h
+++ queueManager.h
@@ -4,9 +4,55 @@
 This is the header file for managing the queue for the operating systems simulation.
 Puugu
 Project Created: 7 October 2017
-Last Edited: 7 October 2017
+Last Edited: 8 October 2017
 *****************************************************************************************************/
 
 #include <iostream>
 
 using namespace std;
+
+#ifndef QUEUEMANAGER_H
+#define QUEUEMANAGER_H
+
+class queueManager {
+private:
+	//create structure for node
+	struct queueNode {
+		queueNode * nextNode;
+		queueNode * prevNode;
+		int dataVal;
+	};
+
+	//create head, tail, current, and destruction pointer
+	queueNode * head;
+	queueNode * tail;
+	queueNode * current;
+	queueNode * godzilla;
+
+public:
+	//constructor
+		/*Hey Future - Puugu, I know you always forget, so when you're looking back at old code trying
+		to figure out why your constructor is giving you an error about explicit type, YOUR FUNCTION
+		NAME HAS TO BE THE SAME AS YOUR CLASS NAME (when making the constructor and desctructor) */
+	queueManager();
+
+	//create node and insert onto head (FIFO)
+	void addNodeFIFO (int dataVal);
+
+	//create node adn insert onto tail (LIFO)
+	void addNodeLIFO(int dataVal);
+
+	//create node and insert via sorting
+		//largest to tail
+	void addNodeSort(int dataVal);
+
+	//create node and insert via priority
+	void addNodePriority(int dataVal, int priorityVal);
+
+	//display queue
+	void displayQueue();
+
+	//destructor
+	~queueManager();
+};
+#endif
