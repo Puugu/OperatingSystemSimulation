a8dafbb94beb1715c38e0c0fd47c49342f947b5b
Puugu <therealckirkham@gmail.com>
2017-10-07 20:31:35 -0500

    add initial project files with project descriptions

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*****************************************************************************************************
+This project is designed to simulate how an operating system uses queues to manage the registers.
+Puugu
+Project Created: 7 October 2017
+Last Edited: 7 October 2017
+*****************************************************************************************************/
+
+#include <iostream>
+#include "queueManager.h"
+
+using namespace std;
+
+int main() {
+	
+	system("pause");
+	return 0;
+}
---
+++ queueManager.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/*****************************************************************************************************
+This is the header file for managing the queue for the operating systems simulation.
+Puugu
+Project Created: 7 October 2017
+Last Edited: 7 October 2017
+*****************************************************************************************************/
